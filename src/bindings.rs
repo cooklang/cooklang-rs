e871ea4ed68ceb25dfc59ec92a04e9e20e5ddfb3
Alexey Dubovskoy <dubovskoy.a@gmail.com>
2023-08-29 09:50:39 +0100

    Implement metadata

--- src/bindings.rs
+++ src/bindings.rs
@@ -1,16 +1,18 @@
 // use crate::*;
 use crate::analysis::*;
-use crate::parser::{parse as rust_parser};
+use crate::parser::{parse as canonical_parse};
 use crate::model::{ComponentKind, Component, Item as ModelItem};
-// use crate::CooklangParser;
 use crate::Converter;
 use crate::Extensions;
-// use analysis::;
+use std::collections::HashMap;
 
 
 #[derive(uniffi::Record, Debug)]
 pub struct CooklangRecipe {
+    metadata: HashMap<String,String>,
     steps: Vec<Step>,
+    ingredients: Vec<Item>,
+    cookware: Vec<Item>,
 }
 
 #[derive(uniffi::Record, Debug)]
@@ -18,13 +20,13 @@ struct Step {
     items: Vec<Item>
 }
 
-#[derive(uniffi::Record, Debug, PartialEq)]
+#[derive(uniffi::Record, Debug, Clone, PartialEq)]
 struct Amount {
     quantity: String,
     units: String
 }
 
-#[derive(uniffi::Enum, Debug, PartialEq)]
+#[derive(uniffi::Enum, Debug, Clone, PartialEq)]
 enum Item {
     Text { value: String },
     Ingredient { name: String, amount: Amount },
@@ -42,7 +44,7 @@ fn into_item(item: ModelItem, recipe: &RecipeContent) -> Item {
                 ComponentKind::IngredientKind => {
                     let igredient = &recipe.ingredients[index];
                     let mut q: String = "".to_string();
-                    let mut u: String = "".to_string();
+                    let u: String = "".to_string();
 
                     if let Some(quantity) = &igredient.quantity {
                         q = quantity.value.to_string();
@@ -82,18 +84,41 @@ fn into_item(item: ModelItem, recipe: &RecipeContent) -> Item {
 }
 
 fn dumb_down_recipe(recipe: &RecipeContent) -> CooklangRecipe {
-    let steps = recipe.sections.clone().into_iter().map(|section|
-        section.steps.into_iter().map(|step|
-            Step { items: step.items.into_iter().map(|item| into_item(item, &recipe)).collect() }
-        )
-    ).flatten().collect();
+    let mut metadata = HashMap::new();
+    let mut steps: Vec<Step> = Vec::new();
+    let mut ingredients: Vec<Item> = Vec::new();
+    let mut cookware: Vec<Item> = Vec::new();
+    let mut items: Vec<Item> = Vec::new();
+
+    (&recipe.sections).iter().for_each(|section| {
+        (&section.steps).iter().for_each(|step| {
+            (&step.items).iter().for_each(|item| {
+                    let i = into_item(item.clone(), &recipe);
+
+                    match i {
+                        Item::Ingredient { name: _, amount: _ } => ingredients.push(i.clone()),
+                        Item::Cookware { name: _ } => cookware.push(i.clone()),
+                        _ => (),
+                    };
+
+                    items.push(i);
+            });
+            // TODO: think how to make it faster as we probably
+            // can switch items content into the step without cloning it
+            steps.push(Step { items: items.clone() });
+            items.clear();
+        })
+    });
+
+    (&recipe.metadata.map).iter().for_each(|(key, value)| {
+        metadata.insert(key.to_string(), value.to_string());
+    });
 
     CooklangRecipe {
-            // metadata: result.metadata,
+            metadata: metadata,
             steps: steps,
-            // ingredients: result.ingredients,
-            // cookware: result.cookware,
-            // timers: result.timers,
+            ingredients: ingredients,
+            cookware: cookware,
     }
 }
 
@@ -102,7 +127,7 @@ pub fn parse(input: String) -> CooklangRecipe {
     let extensions = Extensions::empty();
     let converter = Converter::empty();
 
-    let ast = rust_parser(&input, extensions).take_output().unwrap();
+    let ast = canonical_parse(&input, extensions).take_output().unwrap();
     let result = parse_ast(ast, extensions, &converter, None)
         .take_output()
         .unwrap();
