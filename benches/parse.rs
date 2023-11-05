5dd58ecb2cc1e9aa95e1b52627f7a08be773356c
Francisco J. Sanchez <zheoni@outlook.es>
2023-11-06 00:40:55 +0100

    add new benchmarks
    some of them are just to track internal performance of diferent
    implementations

--- benches/parse.rs
+++ benches/parse.rs
@@ -1,22 +1,41 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 
-use cooklang::CooklangParser;
+use cooklang::{parser::PullParser, CooklangParser, Extensions};
 
 const TEST_RECIPE: &str = include_str!("./test_recipe.cook");
 
 fn complete_recipe(c: &mut Criterion) {
-    let mut group = c.benchmark_group("complete recipe");
-    let parser = CooklangParser::default();
-    let input = black_box(TEST_RECIPE);
-    group.bench_function("cooklang-rs", |b| b.iter(|| parser.parse(input)));
+    let mut group = c.benchmark_group("complete_recipe");
+
+    let canonical = CooklangParser::canonical();
+    let extended = CooklangParser::extended();
+
+    group.bench_with_input("cooklang-rs-canonical", TEST_RECIPE, |b, input| {
+        b.iter(|| canonical.parse(input))
+    });
+    group.bench_with_input("cooklang-rs-extended", TEST_RECIPE, |b, input| {
+        b.iter(|| extended.parse(input))
+    });
+}
+
+fn just_events(c: &mut Criterion) {
+    let mut group = c.benchmark_group("just_events");
+
+    group.bench_with_input("cooklang-rs-extended", TEST_RECIPE, |b, input| {
+        b.iter(|| PullParser::new(input, Extensions::all()).count())
+    });
+    group.bench_with_input("cooklang-rs-canonical", TEST_RECIPE, |b, input| {
+        b.iter(|| PullParser::new(input, Extensions::empty()).count())
+    });
 }
 
 fn just_metadata(c: &mut Criterion) {
-    let mut group = c.benchmark_group("just metadata");
+    let mut group = c.benchmark_group("just_metadata");
     let parser = CooklangParser::default();
-    let input = black_box(TEST_RECIPE);
-    group.bench_function("cooklang-rs", |b| b.iter(|| parser.parse_metadata(input)));
+    group.bench_with_input("cooklang-rs", TEST_RECIPE, |b, input| {
+        b.iter(|| parser.parse_metadata(input))
+    });
 }
 
-criterion_group!(benches, complete_recipe, just_metadata);
+criterion_group!(benches, complete_recipe, just_events, just_metadata);
 criterion_main!(benches);
