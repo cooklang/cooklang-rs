8611d53a110b49e35e0ba2275204f7c4d9879cf4
Francisco J. Sánchez <zheoni@outlook.es>
2024-08-01 00:27:53 +0200

    update example for internal use

--- examples/cook.rs
+++ examples/cook.rs
@@ -1,9 +1,11 @@
+use std::io::Read;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut args = std::env::args();
     let bin = args.next().unwrap();
     let in_file = match args.next() {
         Some(path) => path,
-        None => panic!("Usage: {bin} <input_file> [output_file|STDOUT]"),
+        None => panic!("Usage: {bin} [<input_file>|STDIN] [output_file|STDOUT]"),
     };
     let out_file: Option<Box<dyn std::io::Write>> = match args.next().as_deref() {
         Some("STDOUT") => Some(Box::new(std::io::stdout().lock())),
@@ -11,13 +13,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         None => None,
     };
 
-    let input = std::fs::read_to_string(&in_file)?;
+    let input = match in_file.as_ref() {
+        "STDIN" => {
+            let mut buf = String::new();
+            std::io::stdin().lock().read_to_string(&mut buf)?;
+            buf
+        }
+        path => std::fs::read_to_string(path)?,
+    };
 
     match cooklang::parse(&input).into_result() {
         Ok((recipe, warnings)) => {
             warnings.eprint(&in_file, &input, true)?;
             if let Some(mut out) = out_file {
-                write!(out, "{:#?}", recipe)?;
+                writeln!(out, "{:#?}", recipe)?;
             }
         }
         Err(e) => {
