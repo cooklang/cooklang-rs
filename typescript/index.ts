91eda8ba87d56668d77083482664c8b23a3b27a5
Matt Panzer <panzer.matthew@gmail.com>
2025-08-10 16:32:18 -0400

    Narrow exports

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,1 +1,10 @@
-export * from "./pkg/cooklang_wasm";
+/* High Level Types */
+export { Parser, Recipe, version } from "./pkg/cooklang_wasm";
+export type { ScaledRecipeWithReport } from "./pkg/cooklang_wasm";
+
+/* Types defined by Cooklang syntax */
+export { Section, Step, Ingredient, Cookware, Timer } from "./pkg/cooklang_wasm";
+
+/* Parsed base-types */
+export { Item, Quantity, Value } from "./pkg/cooklang_wasm";
+
