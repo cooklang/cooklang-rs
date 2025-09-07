46c82551621a5ef6730b74adc1b386dca080be03
Michał Moczulski <michalmoczulski1a@gmail.com>
2025-09-07 21:16:55 +0200

    rest of HTML renderer

--- typescript/test/htmlRenderer.test.ts
+++ typescript/test/htmlRenderer.test.ts
@@ -22,7 +22,48 @@ it("renders ingredients", async () => {
     const input = `
     @cat{3}(black)
     `;
-    const output = "<h2>Ingredients:</h2><ul><li class='ingredients'><b>cat</b>: 3 (black)</li></ul>";
+    const output = "<h2>Ingredients:</h2><ul><li><b>cat</b>: 3 (black)</li></ul><hr><b>1. </b>    <span class='ingredient'>cat<i>(3)</i></span>";
+
+    const recipe = new CooklangRecipe(input);
+    const renderer = new HTMLRenderer();
+    expect(renderer.render(recipe)).toEqual(output);
+});
+
+it("renders cookware", async () => {
+    const input = `
+    #cauldron{3}(magic)
+    `;
+    const output = "<h2>Cookware:</h2><ul><li><b>cauldron</b>: 3 (magic)</li></ul><hr><b>1. </b>    <span class='ingredient'>cauldron<i>(3)</i></span>";
+
+    const recipe = new CooklangRecipe(input);
+    const renderer = new HTMLRenderer();
+    expect(renderer.render(recipe)).toEqual(output);
+});
+
+it("renders timer", async () => {
+    const input = `
+    ~eon{5min}
+    `;
+    const output = "<b>1. </b>    <span class='timer'>(eon)<i>(5min)</i></span>";
+
+    const recipe = new CooklangRecipe(input);
+    const renderer = new HTMLRenderer();
+    expect(renderer.render(recipe)).toEqual(output);
+});
+
+it("renders sections and steps", async () => {
+    const input = `
+=== aaa
+    a
+    
+    b
+    
+=== bbb
+    c
+    
+    d
+    `;
+    const output = "<h3>(1) aaa</h3><b>1. </b>    a<b>2. </b>    b<h3>(2) bbb</h3><b>1. </b>    c<b>2. </b>    d";
 
     const recipe = new CooklangRecipe(input);
     const renderer = new HTMLRenderer();
