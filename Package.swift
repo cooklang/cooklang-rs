4cef823c2df5260b1bdb4647fca882d2cc6837dd
Alexey Dubovskoy <dubovskoy.a@gmail.com>
2026-02-24 15:21:52 +0000

    refactor: rename package from cooklang-rs to CooklangParser

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 import class Foundation.ProcessInfo
 
 var package = Package(
-    name: "cooklang-rs",
+    name: "CooklangParser",
     platforms: [
         .iOS(.v15),
     ],
